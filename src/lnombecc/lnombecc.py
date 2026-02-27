714de4ab70ded2bd2fb9e0746a4feda5567084c0
benshi97 <shixubenjamin@gmail.com>
2026-02-27 15:40:25 -0500

    fix path

--- src/lnombecc/lnombecc.py
+++ src/lnombecc/lnombecc.py
@@ -393,7 +393,7 @@ def run_periodic_hf(
             calc_parameters["kpts"] = (1, 1, 1)
 
         # Skip the calculation if "aborting loop because EDIFF is reached"
-        with open(struct.calc.directory / "OUTCAR", "r") as f:
+        with open(calc_folder / "OUTCAR", "r") as f:
             if any("aborting loop because EDIFF is reached" in line for line in f):
                 LOGGER.info(f"Skipping calculation for {calc_key}.")
                 continue
